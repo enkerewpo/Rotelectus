c04a04de8dbe029c3a0cce4037dc6b443be5a876
Kvar_ispw17 <enkerewpo@gmail.com>
2021-03-16 17:56:54 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-'''
-@author Kvar_ispw17
-@email enkerewpo@gmail.com
-'''
-
 import hanlp
 
 if __name__ == '__main__':
+
+    in_file = open('input.txt', 'r')
+    input_str = str(in_file.read())
+
+    out_file = open('output.json', 'w')
     HanLP = hanlp.load(hanlp.pretrained.mtl.OPEN_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
-    HanLP(['2021年 HanLPv2.1为生产环境带来次世代最先进的多语种NLP技术。']).pretty_print()
+    out_str = str(HanLP([input_str]))
+
+    HanLP([input_str]).pretty_print()
+
+    out_file.write(out_str)
+
+    in_file.close()
+    out_file.close()
