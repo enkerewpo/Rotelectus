7519768b29d6b6e7abfb4c34791095242907761b
Kvar_ispw17 <enkerewpo@gmail.com>
2021-03-16 20:10:26 +0800

    update

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import hanlp
 
 if __name__ == '__main__':
 
+    # input file opening
     in_file = open('input.txt', 'r')
     input_str = str(in_file.read())
 
+    # output the json file to $output.json
     out_file = open('output.json', 'w')
-    HanLP = hanlp.load(hanlp.pretrained.mtl.OPEN_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
+    HanLP = hanlp.load(hanlp.pretrained.mtl.UD_ONTONOTES_TOK_POS_LEM_FEA_NER_SRL_DEP_SDP_CON_XLMR_BASE)
     out_str = str(HanLP([input_str]))
 
+    # pretty print the structure in terminal
     HanLP([input_str]).pretty_print()
 
     out_file.write(out_str)
 
+    # file closing
     in_file.close()
     out_file.close()
