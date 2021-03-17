b286e43617fcaff1b007b131131a2297acce07f6
Kvar_ispw17 <enkerewpo@gmail.com>
2021-03-17 12:51:00 +0800

    update

--- main.py
+++ main.py
@@ -4,34 +4,37 @@ import hanlp
 import json
 
 
-def processing():
-    # input file opening
+def build():
     in_file = open('input.txt', 'r')
     input_str = str(in_file.read())
 
-    # output the json file to $output.json
     out_file = open('output.json', 'w')
     HanLP = hanlp.load(hanlp.pretrained.mtl.UD_ONTONOTES_TOK_POS_LEM_FEA_NER_SRL_DEP_SDP_CON_XLMR_BASE)
     out_str = str(HanLP([input_str]))
 
-    # pretty print the structure in terminal
     HanLP([input_str]).pretty_print()
 
     out_file.write(out_str)
 
-    # file closing
     in_file.close()
     out_file.close()
 
 
 if __name__ == '__main__':
 
-    flag = 0
+    flag = 1
     if flag:
-        processing()
+        build()
 
     json_file = open('output.json')
     data = json.load(json_file)
-    for p in data['tok']:
+    cur = 0
+    for p in data['dep']:
         for q in p:
-            print(q)
+            to = q[0] - 1
+            rel = q[1]
+            if (rel == 'nsubj') \
+                    and (data['pos'][0][to] == 'NOUN' or data['pos'][0][to] == 'PROPN') \
+                    and (data['pos'][0][cur] == 'NOUN' or data['pos'][0][cur] == 'PROPN'):
+                print(data['tok'][0][to] + '->' + data['tok'][0][cur])
+            cur += 1
