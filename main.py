87e20b11eff25a40dfdfd9fc7a744f0b121720f2
Kvar_ispw17 <enkerewpo@gmail.com>
2021-03-17 20:59:57 +0800

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def build():
     out_file = open('output.json', 'w')
     HanLP = hanlp.load(hanlp.pretrained.mtl.UD_ONTONOTES_TOK_POS_LEM_FEA_NER_SRL_DEP_SDP_CON_XLMR_BASE)
     out_str = str(HanLP([input_str]))
+    HanLP([input_str]).pretty_print()
 
     out_file.write(out_str)
     out_file.close()
@@ -23,8 +24,7 @@ if __name__ == '__main__':
     input_str = str(in_file.read())
     in_file.close()
 
-
-    flag = 0
+    flag = 1
     if flag:
         build()
 
@@ -45,7 +45,20 @@ if __name__ == '__main__':
         if tok_c == '是':
             equ = i
             break
-    print('[LOG] Got relation keyword \'是\' at position:', equ)
+    if equ != -1:
+        print('[LOG::EQU-REL] Got relation keyword \'是\' at position:', equ)
+        equt = data['dep'][0][equ]
+        argv1 = toks[equt[0] - 1]
+        print('[LOG::EQU-REL] Found target', argv1)
+        deps = data['dep'][0]
+        id_argv0 = -1
+        for i in range(len(deps)):
+            x = deps[i]
+            if x[0] == equt[0] and x[1] == 'nsubj':
+                id_argv0 = i
+                break
+        argv0 = toks[id_argv0]
+        print('[LOG::EQU-REL] Found \'=\' relation:\n\n', argv0, '=', argv1)
     json_file.close()
-    print('[LOG] All jobs finished and exiting ...')
+    print('\n\n[LOG] All jobs finished and exiting ...')
     print('-------------------------------------------------------\n')
